bdd3e80007d31968867e743149de41defa456ede
Jay Prajapati <jayprajapati9900@gmail.com>
2022-12-06 01:35:29 -0500

    launch file upload

---
+++ launch/walker_launch.py
@@ -0,0 +1,40 @@
+"""Launch file to initiate the walker algorithm for the turtlebot3 in gazebo and record the bag file"""
+import os
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    """Method to launch the nodes in the package with bag record flag"""
+    bag_record = LaunchConfiguration('bag_record')
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'bag_record',
+            default_value='False'
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch'),
+            '/turtlebot3_world.launch.py'])
+        ),
+
+        Node(
+            package='walker_algo',
+            executable='walker',
+        ),
+
+        ExecuteProcess(
+        condition=IfCondition(bag_record),
+        cmd=[
+            'd ../results/bag_files', '&&', 'ros2', 'bag', 'record', '-a', '-x "/camera.+"'
+        ],
+        shell=True
+        )
+
+    ])
